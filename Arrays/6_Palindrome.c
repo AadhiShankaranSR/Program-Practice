166afe7efc644fe03cee3478b4e04b8ff5eac3ba
Aadhishankaran S R <149928382+AadhiShankaranSR@users.noreply.github.com>
2024-03-04 19:45:24 +0530

    C3

--- Arrays/6_Palindrome.c
+++ Arrays/6_Palindrome.c
@@ -1,16 +1,26 @@
 #include <stdio.h>
 
-int main() {
+int main()
+{
     int num;
     printf("Enter a Number: ");
-    scanf("%d",&num);
+    scanf("%d", &num);
     int numCopy = num;
+    int reverse = 0;
     while (num != 0)
     {
-        static int reverse = 0;
         reverse = (reverse * 10) + (num % 10);
         num = num / 10;
     }
-    
+
+    if (numCopy == reverse)
+    {
+        printf("%d is a Palindrome", reverse);
+    }
+    else
+    {
+        printf("%d is not a Palindrome", numCopy);
+    }
+
     return 0;
 }
